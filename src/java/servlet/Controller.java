e88a8a9d49822f6aa90e9c8aedcccc794e301f1e
Gonzalo <Gonzalo@DESKTOP-CG8PO0C>
2019-01-30 12:24:13 +0100

    Merge develop
    Conflicts:
    	web/index.html

--- src/java/servlet/Controller.java
+++ src/java/servlet/Controller.java
@@ -7,11 +7,16 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import jpautil.JPAUtil;
 
 /**
  *
@@ -31,18 +36,24 @@ public class Controller extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Controller</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Controller at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+        HttpSession session = request.getSession();
+        RequestDispatcher dispatcher;
+        
+        String op;
+        String sql;
+        Query query;
+        EntityManager em = null;
+        
+        if (em == null) {
+            em = JPAUtil.getEntityManagerFactory().createEntityManager();
+            session.setAttribute("em", em);
+        }
+        
+        op = request.getParameter("op");
+        
+        if (op.equals("inicio")) {
+
         }
     }
 
