4a0d99e97fa19f44b0d2328d389978dae0952718
Diurno <Diurno@DESKTOP-4J90N74>
2019-02-05 19:20:03 +0100

    Commit Jonay
    Cambios en modales.html y myJs y añadida la pagina apuestas

---
+++ web/apuestas.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : apuestas
+    Created on : 05-feb-2019, 18:59:50
+    Author     : Usuario
+--%>
+
+<%@page import="java.util.List"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<h6>Titulo del partido</h6>
+<table>
+    <thead>
+      <tr>
+          <th>Apuesta</th>
+          <th>Numero de puestas</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>GLocal - GVisitante</td>
+        <td>Numero Apuestas</td>
+      </tr>
+    </tbody>
+</table>
--- web/js/myjs.js
+++ web/js/myjs.js
@@ -4,10 +4,39 @@ $(document).ready(function() {
 });
 
 function init(){
+    loadApuestas()
     $('#modal-login').modal();
     $('#modal-listaApuestas').modal();
     $('#modal-apostar').modal();
     $('.modal-trigger').modal();
+    apostar()
+}
+
+function loadApuestas(){
+    $('#modal-listaApuestas').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var idPartido = button.data('id');
+        $.ajax({
+            type: "POST",
+            url: "Controller?op=infoapuestas&idpartido=" + idPartido,
+            success: function (info) {
+                $("#div-apuestas").html(info);
+            }
+        });
+    });
+}
+
+function apostar(){
+    $('#modal-apostar').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget) // Button that triggered the modal
+        var idpartido = button.data('id')
+        var partido=button.data('whatever')// Extract info from data-* attributes
+        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+        var modal = $(this)
+        modal.find('#idPartido').val(idpartido)
+        modal.find('#partido').val(partido)
+      })
 }
 
 
